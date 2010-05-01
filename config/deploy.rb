ffdbf93d4749180460345e45a0d436ef223582b1
Dave Files <dfiles@gmail.com>
2010-04-30 23:47:10 -0500

    Corrected the deploy.rb file Updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,13 +19,14 @@ default_run_options[:pty] = true
   role :web, "www.chs1971.com" 
   role :db,  "www.chs1971.com", :primary => true
 
-  namespace :deploy do
-    desc "Restart Application"
-    task :restart, :roles => :app, :except => {:no_release => true}  do
-      run "touch #{current_path}/tmp/restart.txt"
-    end
-    desc "Start Application -- not needed for Passenger"
-    task :start, :roles => :app do
-      # nothing -- need to override default cap start task when using Passenger
-    end
+ namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
