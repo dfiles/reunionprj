f5fae49806ea8edf3489684c22fc549f4bc7355f
Dave Files <dfiles@gmail.com>
2010-05-11 21:49:25 -0500

    Updated the contact us email mailer. Setup actionmailer...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,15 +14,9 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+# config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.delivery_method = :smtp
 
 # set up email server
-
-#ActionMailer::Base.delivery_method = :smtp
-#ActionMailer::Base.smtp_settings = {
-#  :address => 'smtp.gmail.com',
-#  :port => "587",
-#  :domain => 'localhost.localdomain'
-#}
-
-#CONTACT_RECIPIENT = 'chsclass1971@gmail.com'
+CONTACT_RECIPIENT = 'chsclass1971@gmail.com'
