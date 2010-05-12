3d0eed5285c84edf32f64e8b8b7b8138d5769ccb
Dave Files <dfiles@gmail.com>
2010-05-11 21:58:24 -0500

    updated prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = false
+CONTACT_RECIPIENT = 'chsclass1971@gmail.com'
 
 # Enable threaded mode
 # config.threadsafe!
